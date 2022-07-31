78e363c6f44775ced21925ee122de8a7b5bdb67f
Eusebio Simango <eusebiosimango14@gmail.com>
2022-07-31 15:08:28 +0200

    Basics

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {  BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      Starting...
+    </div>
+  )
+}
+
+export default App
