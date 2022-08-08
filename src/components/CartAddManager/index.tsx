0377f30b86a55238aa644a07523ef8079e9ebf54
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-08 15:54:19 +0200

    outline on focus

--- src/components/CartAddManager/index.tsx
+++ src/components/CartAddManager/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './index.module.css'
+import Cart from '../../assets/icon-cart.svg'
+
 
 const CartAddManager = () => {
+
+	const [itemQuantity, setItemQuantity] = useState<Number>(0)
+
 	return (
-		<div>
-			
+		<div className={styles.cartAddManager}>
+			<div className={styles.controler}></div>
+			<button className={styles.addToCart}>
+				<img src={Cart} alt="Cart Icon"/>
+				Add to cart
+			</button>
 		</div>
 	)
 }
