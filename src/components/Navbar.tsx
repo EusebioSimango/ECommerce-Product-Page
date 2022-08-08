deb46e55084b9e107681617e703c48684aa1bbb8
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-08 14:46:32 +0200

    after on hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import Cart from "../assets/icon-cart.svg"
-import Avatar from "../assets/image-avatar.png"
-import Logo from "../assets/logo.svg"
+import Logo from '../assets/logo.svg'
+import Cart from '../assets/icon-cart.svg'
+import Avatar from '../assets/image-avatar.png'
+
+
+// import { Container } from './styles';
 
 const Navbar: React.FC = () => {
   return (
   <header className='container'>
     <div className="left">
-      <a className='logo' href="./"> 
-        <img src={Logo}/>
+      <a className='logo' href="/">
+        <img src={Logo} alt="Sneakers's Logo" />
       </a>
       <nav>
         <ul>
-          <li><a href="#">Collections</a></li>
+          <li ><a href="#">Collections</a></li>
           <li><a href="#">Men</a></li>
-          <li><a className="active" href="#">Women</a></li>
+          <li className="active"><a href="#">Women</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
@@ -22,11 +25,10 @@ const Navbar: React.FC = () => {
     </div>
     <div className="right">
       <div className="cart">
-        <img alt="Cart Icon" src={Cart}/>
+        <img src={Cart} alt="Cart Icon" />
       </div>
       <div className="profile">
-        <img alt="Avatar" src={Avatar}/>
-        
+        <img src={Avatar} alt="User's Avatar"/>
       </div>
     </div>
   </header>
