be82a9c40b6cbfb9320cf80e5ab4e6a7badf3a61
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-08 20:19:12 +0200

    tu

--- src/components/ImageViewer/index.tsx
+++ src/components/ImageViewer/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
-
+import Prod1 from './../../assets/image-product-1.jpg'
 import styles from './index.module.css'
-
+import Prod1Thumbnail from './../../assets/image-product-1-thumbnail.jpg'
 
 const ImageViewer = () => {
   return (
-    <>
-      ...
-    </>
+    <div className={styles.imageViewer}>
+      <div className={styles.currentImage}>
+        <img className={styles.active} src={Prod1}/>
+      </div>
+      <div className={styles.imageNavegation}>
+        <div className={styles.imageThumbnail}>
+          <img src={Prod1Thumbnail}/>
+        </div>
+        <div className={styles.imageThumbnail}>
+          <img src={Prod1Thumbnail}/>
+        </div>
+        <div className={styles.imageThumbnail}>
+          <img src={Prod1Thumbnail}/>
+        </div>
+        <div className={styles.imageThumbnail}>
+          <img src={Prod1Thumbnail}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
